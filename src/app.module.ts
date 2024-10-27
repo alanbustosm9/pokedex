6c74dd8d87712866aa609600f88d981371b7834d
Alan Bustos <alanbustox46@gmail.com>
2024-10-27 02:17:12 -0300

    fix: Update docker-compose.yaml and Mongoose connection string
    - Update the port mapping in docker-compose.yaml to remove the quotes around the host port.
    - Update the Mongoose connection string in app.module.ts to include the port number.

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { MongooseModule } from '@nestjs/mongoose';
       rootPath: join(__dirname, '..', 'public'),
     }),
 
-    MongooseModule.forRoot('mongodb://localhost/27017/nest-pokemon'),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
 
     PokemonModule,
   ],
